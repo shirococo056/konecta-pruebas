40576edd72dff4e9d2a9e80dc89ef68a99c1843c
shirococo056 <91107435+shirococo056@users.noreply.github.com>
2021-10-14 20:04:02 -0300

    hgsdgs

--- src/components/header.js
+++ src/components/header.js
@@ -14,7 +14,7 @@ export default function Header() {
 
   return (
     <header className="h-16 bg-white border-b border-gray-primary mb-8 px-4 lg:px-0">
-      <div className="container mx-auto max-w-screen-lg h-full">
+      <div className="container mx-auto max-w-full h-full">
         <div className="flex justify-between h-full">
           <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
             <h1 className="flex justify-center w-full">
